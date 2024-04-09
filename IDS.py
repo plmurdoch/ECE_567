24516f5d8f9a10117d9a60ba85a255e2b24e3239
plmurdoch <pmurdoch@uvic.ca>
2024-04-08 22:18:07 -0700

    changing to binary classifier as either dos or not

--- IDS.py
+++ IDS.py
@@ -6,15 +6,19 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
+from sklearn.metrics import RocCurveDisplay
+from sklearn.metrics import auc
 import matplotlib.pyplot as plt
 
 def Training_Testing_Model(file, output_name):
     Parse_CSV = pandas.read_csv(file)
     Parse_CSV = Parse_CSV.replace([-numpy.inf, numpy.inf], numpy.nan)
     Parse_CSV = Parse_CSV.dropna()
+    Parse_CSV = Parse_CSV.replace(["BENIGN"], 0)
+    Parse_CSV = Parse_CSV.replace(["DoS Hulk","DoS Slowhttptest","DoS GoldenEye","DoS slowloris"], 1)
     X_val = Parse_CSV.iloc[:,list(range(7,84))].values
     Y_val = Parse_CSV.iloc[:,-1].values
-    Machine = KNeighborsClassifier(n_neighbors = 7) #7 good approximation without over fitting most accurate for KNN with most reliable accuracy 97% with inf and nan entities dropped
+    Machine = KNeighborsClassifier(n_neighbors = 7) #7 good approximation without over fitting most accurate for KNN with most reliable accuracy 95-97% with inf and nan entities dropped
     scale = StandardScaler()
     x_training, x_testing, y_training, y_testing = train_test_split(X_val, Y_val, test_size = 0.2, stratify=Y_val)
     x_train = scale.fit_transform(x_training)
@@ -22,6 +26,8 @@ def Training_Testing_Model(file, output_name):
     Machine.fit(x_train, y_training)
     y_pred = Machine.predict(x_test)
     print(f"Model Accuracy:{accuracy_score(y_testing, y_pred)}")
+    RocCurveDisplay.from_predictions(y_testing, y_pred)
+    plt.show()
     joblib.dump(Machine, output_name)
 
 def Validating_Data(file, input):
